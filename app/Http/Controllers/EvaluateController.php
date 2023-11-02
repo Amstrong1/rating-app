387338cf19bcd042d71817de27c841146a99adb9
Amstrong1 <lewisamstrongh@gmail.com>
2023-11-02 08:37:50 +0100

    show view

--- app/Http/Controllers/EvaluateController.php
+++ app/Http/Controllers/EvaluateController.php
@@ -11,29 +11,29 @@ class EvaluateController extends Controller
         $structure = Auth::user()->structure;
         return view('app.evaluate.index', [
             'evaluates' => $structure->rates()->get(),
-            'my_actions' => $this->evaluate_actions(),
-            'my_attributes' => $this->evaluate_columns(),
+            // 'my_actions' => $this->evaluate_actions(),
+            // 'my_attributes' => $this->evaluate_columns(),
         ]);
     }
 
-    private function evaluate_columns()
-    {
-        $columns = (object) array(
-            'user' => 'Employé',
-            'question' => 'Question',
-            'answer_formatted' => 'Réponse',
-            'description' => "Description",
-            'rate_date' => "Date",
-        );
-        return $columns;
-    }
+    // private function evaluate_columns()
+    // {
+    //     $columns = (object) array(
+    //         'user' => 'Employé',
+    //         'question' => 'Question',
+    //         'answer_formatted' => 'Réponse',
+    //         'description' => "Description",
+    //         'rate_date' => "Date",
+    //     );
+    //     return $columns;
+    // }
 
-    private function evaluate_actions()
-    {
-        $actions = (object) array(
-            'edit' => 'Modifier',
-            'delete' => "Supprimer",
-        );
-        return $actions;
-    }
+    // private function evaluate_actions()
+    // {
+    //     $actions = (object) array(
+    //         'edit' => 'Modifier',
+    //         'delete' => "Supprimer",
+    //     );
+    //     return $actions;
+    // }
 }
