e35800ac7dc7e56725068662706ad0cf05325e76
Amstrong1 <lewisamstrongh@gmail.com>
2023-11-02 11:33:28 +0100

    1 bug fix

--- app/Http/Controllers/EvaluateController.php
+++ app/Http/Controllers/EvaluateController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class EvaluateController extends Controller
 {
     public function index()
     {
         $structure = Auth::user()->structure;
+        $users = $structure->users()->get();
+        $evaluates = new EloquentCollection();
+        foreach ($users as $user) {
+            $evaluates[] = $structure->rates()->where('user_id', $user->id)->orderBy('id', 'desc')->first();
+        }
+        // $evaluates = $evaluates->collapse();
+        // $evaluates = $structure->rates()->get();
+        // $userEvaluate = $evaluates->where('answer', true);
+
+        // dd($structure->rates()->where('answer', true)->groupBy('user_id')->get());
+        // dd($structure->rates()->where('answer', true)->groupBy('user_id')->get('user_id'));
         return view('app.evaluate.index', [
-            'evaluates' => $structure->rates()->get(),
+            // 'evaluates' => $structure->rates()->groupBy('user_id')->get(),
+            'evaluates' => $evaluates,
             // 'my_actions' => $this->evaluate_actions(),
             // 'my_attributes' => $this->evaluate_columns(),
         ]);
--- resources/views/app/evaluate/index.blade.php
+++ resources/views/app/evaluate/index.blade.php
@@ -9,63 +9,70 @@
                     <div class="mt-4">
                         {{-- <x-tables.default :resources="$evaluates" :mattributes="$my_attributes" type="evaluate" :mactions="$my_actions" /> --}}
                         <div style="background-color: #00558b" class="text-white p-4 border border-slate-500">
-                            @if ($evaluates->count() !== 0)
-                            <h2>Trier par date de publication</h2>
+                            @if ($evaluates !== null && $evaluates->count() !== 0)
+                                <h2>Trier par date de publication</h2>
                             @else
-                            <h2>Aucun élément à afficher</h2>
+                                <h2>Aucun élément à afficher</h2>
                             @endif
                         </div>
                         @foreach ($evaluates as $evaluate)
-                            <div class="grid grid-cols-12">
-                                <div style="background-color: #00558b; border-color: #cae1f1; border-right-color: #09beaf"
-                                    class="flex flex-col justify-center items-center text-white col-span-2 p-4 border-2 border-r-4">
-                                    <span class="text-2xl font-bold my-2">
-                                        {{ $evaluate->user->rates()->where('answer', true)->count() *(10 / $evaluate->user->rates()->count()) }}
-                                        /
-                                        {{ $evaluate->user->rates()->count() * (10 / $evaluate->user->rates()->count()) }}
-                                    </span>
-                                    <div class="flex flex-row my-2">
-                                        @for ($i = 0;
+                            @if ($evaluate !== null)
+                                <div class="grid grid-cols-12">
+                                    <div style="background-color: #00558b; border-color: #cae1f1; border-right-color: #09beaf"
+                                        class="flex flex-col justify-center items-center text-white col-span-2 p-4 border-2 border-r-4">
+                                        <span class="text-2xl font-bold my-2">
+                                            {{ $evaluate->user->rates()->where('answer', true)->count() *(10 / $evaluate->user->rates()->count()) }}
+                                            /
+                                            {{ $evaluate->user->rates()->count() * (10 / $evaluate->user->rates()->count()) }}
+                                        </span>
+                                        <div class="flex flex-row my-2">
+                                            @for ($i = 0;
     $i <
-    $evaluate->user->rates()->where('answer', true)->count() *
-        (5 / $evaluate->user->rates()->count());
-    $i++)
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                                            </svg>
-                                        @endfor
-
-                                        @for ($i = 0;
-    $i <
-    4 -
+    ceil(
         $evaluate->user->rates()->where('answer', true)->count() *
-            (5 / $evaluate->user->rates()->count());
+            (5 / $evaluate->user->rates()->count()),
+    );
     $i++)
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                                            </svg>
-                                        @endfor
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="orange"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="w-4 h-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                                </svg>
+                                            @endfor
+
+                                            @for ($i = 0;
+    $i <
+    floor(
+        $evaluate->user->rates()->where('answer', false)->count() *
+            (5 / $evaluate->user->rates()->count()),
+    );
+    $i++)
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="w-4 h-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                                </svg>
+                                            @endfor
+                                        </div>
+                                        <span class="my-2">
+                                            {{ $evaluate->rate_date }}
+                                        </span>
                                     </div>
-                                    <span class="my-2">
-                                        {{ $evaluate->rate_date }}
-                                    </span>
+                                    <div class="col-span-10 p-4 border-2" style="border-color: #cae1f1;">
+                                        <div>
+                                            {{ $evaluate->user->name }}
+                                        </div>
+                                        <div>
+                                            {{ $evaluate->user->place->name . ' - ' . $evaluate->rate_date }}
+                                        </div>
+                                        <div class="italic">
+                                            {{ $evaluate->description ?? 'Le client n\'a laissé aucun commentaire' }}
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-span-10 p-4 border-2" style="border-color: #cae1f1;">
-                                    <div>
-                                        {{ $evaluate->user->name }}
-                                    </div>
-                                    <div>
-                                        {{ $evaluate->user->place->name . ' - ' . $evaluate->rate_date }}
-                                    </div>
-                                    <div class="italic">
-                                        {{ $evaluate->description ?? 'Le client n\'a laissé aucun commentaire' }}
-                                    </div>
-                                </div>
-                            </div>
+                            @endif
                         @endforeach
                     </div>
                 </div>
